0433e077bb941afcdcc54625d96f61722d7ea7da
Yayira Dz <yayirakdzamesi@cedarville.edu>
2019-11-12 16:52:14 -0500

    added more word list generator things

--- src/password_crackers/Dictionary_Generator.java
+++ src/password_crackers/Dictionary_Generator.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 public class Dictionary_Generator {
 
-    public List<String> getUniqueBibleWords(String fileName){
+    public List<String> getUniqueBibleWords(String fileName) {
 
         FileInputStream fis = null;
         DataInputStream dis = null;
@@ -23,11 +23,11 @@ public class Dictionary_Generator {
             dis = new DataInputStream(fis);
             br = new BufferedReader(new InputStreamReader(dis));
             String line = null;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 StringTokenizer st = new StringTokenizer(line, " ,.;:\"");
-                while(st.hasMoreTokens()){
+                while (st.hasMoreTokens()) {
                     String tmp = st.nextToken().toLowerCase();
-                    if(!wordList.contains(tmp)){
+                    if (!wordList.contains(tmp)) {
                         wordList.add(tmp);
                     }
                 }
@@ -36,10 +36,59 @@ public class Dictionary_Generator {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally{
-            try{if(br != null) br.close();}catch(Exception ex){}
+        } finally {
+            try {
+                if (br != null) br.close();
+            } catch (Exception ex) {
+            }
         }
         return wordList;
+    }
+
+    public void appendSymbolsToWords(String fileName) throws IOException {
+        /*
+            String[] symbolsToAppend = {!,@,#,$,%,^,&,*,(,),_};
+
+            BufferedWriter writer = new BufferedWriter(
+                                        new FileWriter("/Users/yiradz/College/JUN_SEM1/SECURITY/PA4/src/password_crackers/symbolAppendedWordList.txt"", true)  //Set true for append mode
+                                    );
+            // For every line of the file, add the symbol to the end of that line.
+            while(fileName.hasNext()){
+            for (int i = 0; i < symbolsToAppend.length(); i ++){
+            // Append the ith symbol in the array to every line in the file
+            writer.write(symbolsToAppend[i]);
+                }
+            }
+            writer.close();
+        */
+    }
+
+    public void prependSymbolsToWords(String fileName) throws IOException {
+        // For every line of the file, add the symbol to the beginning of that line.
+    }
+
+    public void l33tSp34kTranslation(String fileName){
+        // Search in given file and replace E -> 3,  o -> 0, a -> @, $" for "s", "1" for "l" and so on
+        /*
+        Path path = Paths.get(fileName);
+        Charset charset = StandardCharsets.UTF_8;
+
+        String content = new String(Files.readAllBytes(path), charset);
+        content = content.replaceAll("foo", "bar");
+        Files.write(path, content.getBytes(charset));
+
+        OR
+
+         BufferedWriter writer = new BufferedWriter(
+                                        new FileWriter("/Users/yiradz/College/JUN_SEM1/SECURITY/PA4/src/password_crackers/l33tSp34kTranslationWordList.txt"", true)  //Set true for append mode
+                                    );
+        for (String fn : fileName) {
+            String s = new String(Files.readAllBytes(Paths.get(fn)));
+            s = s.replace("E", "3");
+                writer.write(s);
+            }
+        }
+         */
     }
 
     public static void main(String a[]) throws IOException {
@@ -48,8 +97,9 @@ public class Dictionary_Generator {
         List<String> wordList = distinctFileWords.getUniqueBibleWords("/Users/yiradz/College/JUN_SEM1/SECURITY/PA4/src/password_crackers/PA4_Word_List.txt");
         BufferedWriter uniqueBibleWordList = new BufferedWriter(new FileWriter("/Users/yiradz/College/JUN_SEM1/SECURITY/PA4/src/password_crackers/uniqueBibleWordList.txt"));
 
-        for(String str : wordList){
+        for (String str : wordList) {
             uniqueBibleWordList.write(str);
+            uniqueBibleWordList.write("\n");
         }
         uniqueBibleWordList.close();
     }
